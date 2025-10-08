3417fe30240ba49750104ab57a1a57fc92339d3f
Andrew Dirksen <andrew@dirksen.com>
2025-10-07 18:54:18 -0700

    try some crazy biz to get nix builds working in container; this works now, needs cleanup

--- default.nix
+++ default.nix
@@ -7,6 +7,27 @@ let
   };
   pkgs = import nixpkgs_src { };
 
+  docker-nixpkgs = pkgs.fetchFromGitHub {
+    owner = "nix-community";
+    repo = "docker-nixpkgs";
+    rev = "64a3352bbfcca09a04774880951b0030e8f01409";
+    sha256 = "sha256-637nBKXQmFcYV/gg+ivWOBcPTdwPTlI8aBsUZ9vYYLI=";
+  };
+
+  pkgs-with-docker-nixpkgs-overlay =
+    pkgs.appendOverlays [ (import "${docker-nixpkgs}/overlay.nix") ];
+
+  nix-image = pkgs-with-docker-nixpkgs-overlay.docker-nixpkgs.nix.override {
+    # extraContents = [ codex ];
+    # extraEnv = [
+    #   "NIX_PATH=nixpkgs=${nixpkgs_src}"
+    #   "NIX_CONFIG=build-users-group = root"
+    #   "HOME=/root"
+    #   "CARGO_TARGET_DIR=/root/cargo-target"
+    #   "UV_VENV_DIR=/root/uv-venv"
+    # ];
+  };
+
   codex = import ./codex.nix { inherit pkgs; };
 
   allowPodmanLoad = pkgs.writeText "podman-policy.json" (builtins.toJSON {
@@ -22,6 +43,7 @@ let
       mkdir -p $out/tmp
     '';
   });
+
   link_loader = pkgs.runCommand "link-loader" { } ''
     # common tools need /lib64/ld-linux-x86-64.so.2 to exist, this is what nix-ld is for
     # I couldn't find the the derivation for the ld-linux-x86-64.so.2 from nix-ld so I
@@ -31,103 +53,9 @@ let
     ln -s ${pkgs.glibc}/lib/ld-linux-x86-64.so.2 $out/lib64/ld-linux-x86-64.so.2
   '';
 
-  # Add shared libraries for uv’s custom Python below:
-  image_packages = [
-    codex
-    # pkgs.cargo
-    # pkgs.clippy
-    # pkgs.rustfmt
-    pkgs.git
-    pkgs.curl
-    # pkgs.dnsutils
-    # pkgs.fzf
-    # pkgs.gh
-    # pkgs.gnupg
-    pkgs.jq
-    # pkgs.less
-    # pkgs.man-db
-    pkgs.procps
-    pkgs.psmisc
-    # pkgs.unzip
-    pkgs.ripgrep
-    pkgs.bash
-    pkgs.coreutils-full
-    # pkgs.clang
-    pkgs.gnused
-    pkgs.gawk
-    pkgs.python3
-    pkgs.gnugrep
-    pkgs.file
-    pkgs.findutils
-    pkgs.just
-    # pkgs.cmake
-    pkgs.nix
-    pkgs.which
-    # pkgs.gnutar
-    pkgs.uv
-    # pkgs.patchelf
-  ];
-
-  common_shared_libs = [
-    pkgs.glibc
-    pkgs.zlib
-    pkgs.bzip2
-    pkgs.xz
-    pkgs.ncurses
-    pkgs.openssl
-    pkgs.gdbm
-    pkgs.sqlite
-    pkgs.readline
-    pkgs.libffi
-    pkgs.libnsl
-  ];
-
-  image_extras = [
-    pkgs.dockerTools.usrBinEnv
-    pkgs.dockerTools.binSh
-    pkgs.dockerTools.caCertificates
-    pkgs.dockerTools.fakeNss
-    empty_tmpdir
-    link_loader
-    # pkgs.nix-ld
-  ];
-  all_image_packages = image_packages ++ common_shared_libs ++ image_extras;
-
-  env = pkgs.buildEnv {
-    name = "dev-packages";
-    paths = all_image_packages;
-  };
-
-  write_container = pkgs.dockerTools.streamLayeredImage {
-    name = "sandbot-devshell";
-    tag = "sandbot-devshell";
-    contents = env;
-
-    includeStorePaths = true;
-
-    config = {
-      WorkingDir = "/workdir";
-      Env = [
-        "PATH=/usr/local/bin:/usr/bin:/bin"
-        "NIX_PATH=nixpkgs=${nixpkgs_src}"
-        "NIX_CONFIG=build-users-group = root"
-        # "NIX_LD_LIBARY_PATH=${pkgs.lib.makeLibraryPath [ env ]}"
-        "LD_LIBARY_PATH=${pkgs.lib.makeLibraryPath [ env ]}"
-        "HOME=/root"
-        "CARGO_TARGET_DIR=/root/cargo-target"
-        "UV_VENV_DIR=/root/uv-venv"
-      ];
-    };
-  };
-
-  sandbot-load = pkgs.writeShellScriptBin "sandbot-load" ''
+  codex-wrapper = pkgs.writeShellScriptBin "codex-wrapper" ''
     set -ueo pipefail
-    ${write_container} | "${pkgs.podman}/bin/podman" load --signature-policy ${allowPodmanLoad}
-  '';
-
-  sandbot-codex = pkgs.writeShellScriptBin "sandbot-codex" ''
-    set -ueo pipefail
-    exec ${sandbot}/bin/sandbot env codex \
+    exec codex \
       --dangerously-bypass-approvals-and-sandbox \
       -c 'approval_policy=on-failure' \
       -c 'sandbox_mode=danger-full-access' \
@@ -138,11 +66,166 @@ let
       "$@"
   '';
 
-  sandbot = pkgs.writeShellScriptBin "sandbot" ''
+  # Add shared libraries for uv’s custom Python below:
+  image_packages = [
+    # codex-wrapper
+    # codex
+    # # pkgs.cargo
+    # # pkgs.clippy
+    # # pkgs.rustfmt
+    # pkgs.git
+    # pkgs.curl
+    # # pkgs.dnsutils
+    # # pkgs.fzf
+    # # pkgs.gh
+    # # pkgs.gnupg
+    # pkgs.jq
+    # # pkgs.less
+    # # pkgs.man-db
+    # pkgs.procps
+    # pkgs.psmisc
+    # # pkgs.unzip
+    # pkgs.ripgrep
+    # pkgs.bash
+    # pkgs.coreutils-full
+    # # pkgs.clang
+    # pkgs.gnused
+    # pkgs.gawk
+    # pkgs.python3
+    # pkgs.gnugrep
+    # pkgs.file
+    # pkgs.findutils
+    # pkgs.just
+    # # pkgs.cmake
+    # pkgs.nix
+    # pkgs.which
+    # # pkgs.gnutar
+    # pkgs.uv
+    # # pkgs.patchelf
+
+    pkgs.nix
+    pkgs.coreutils
+    pkgs.bash
+  ];
+
+  common_shared_libs = [
+    # pkgs.glibc
+    # pkgs.zlib
+    # pkgs.bzip2
+    # pkgs.xz
+    # pkgs.ncurses
+    # pkgs.openssl
+    # pkgs.gdbm
+    # pkgs.sqlite
+    # pkgs.readline
+    # pkgs.libffi
+    # pkgs.libnsl
+  ];
+
+  image_extras = [
+    # # ./root
+    pkgs.dockerTools.usrBinEnv
+    pkgs.dockerTools.binSh
+    pkgs.dockerTools.caCertificates
+    pkgs.dockerTools.fakeNss
+    empty_tmpdir
+    # link_loader
+    # # pkgs.nix-ld
+  ];
+  all_image_packages = image_packages ++ common_shared_libs ++ image_extras;
+
+  env = pkgs.buildEnv {
+    name = "dev-packages";
+    paths = all_image_packages;
+  };
+
+  write_container = pkgs.dockerTools.streamLayeredImage {
+    # fromImage = nix-image;
+    name = "sandbot-devshell";
+    tag = "sandbot-devshell";
+    contents = env;
+
+    # includeStorePaths = true;
+
+    config = {
+      WorkingDir = "/workdir";
+      Env = [
+        "PATH=/usr/local/bin:/usr/bin:/bin"
+        "NIX_PATH=nixpkgs=${nixpkgs_src}"
+        "NIX_CONFIG=build-users-group = root"
+        "HOME=/root"
+        "CARGO_TARGET_DIR=/root/cargo-target"
+        "UV_VENV_DIR=/root/uv-venv"
+      ];
+    };
+  };
+
+  # container = pkgs.dockerTools.buildImage {
+  #   # fromImage = nix-image;
+  #   name = "sandbot-devshell";
+  #   tag = "sandbot-devshell";
+  #   contents = env;
+
+  #   # includeStorePaths = true;
+
+  #   runAsRoot = ''
+  #     #!${pkgs.runtimeShell}
+  #     ${pkgs.dockerTools.shadowSetup}
+  #     groupadd -r redis
+  #     useradd -r -g redis redis
+  #     mkdir /data
+  #     chown redis:redis /data
+  #   '';
+
+  #   config = {
+  #     WorkingDir = "/workdir";
+  #     Env = [
+  #       "PATH=/usr/local/bin:/usr/bin:/bin"
+  #       "NIX_PATH=nixpkgs=${nixpkgs_src}"
+  #       "NIX_CONFIG=build-users-group = root"
+  #       "HOME=/root"
+  #       "CARGO_TARGET_DIR=/root/cargo-target"
+  #       "UV_VENV_DIR=/root/uv-venv"
+  #     ];
+  #   };
+  # };
+
+  # write_container = pkgs.writeShellScript "write-container" ''
+  #   cat ${container}
+  # '';
+  # write_container = pkgs.writeShellScript "write-container" ''
+  #   cat ${nix-image}
+  # # '';
+
+  sandbot-load = pkgs.writeShellScriptBin "sandbot-load" ''
     set -ueo pipefail
-    exec "${pkgs.podman}/bin/podman" run --rm -it -e OPENAI_API_KEY -v "$(pwd):/workdir" "sandbot-devshell:sandbot-devshell" "$@"
+    ${write_container} | "${pkgs.podman}/bin/podman" load --signature-policy ${allowPodmanLoad}
+  '';
+
+  sandbot-create = pkgs.writeShellScriptBin "sandbot-create" ''
+    set -ueo pipefail
+    if [ $# -ne 1 ]; then
+      echo "Usage: $0 <container-name>"
+      exit 1
+    fi
+    container_name="$1"
+    ${pkgs.podman}/bin/podman create -it --replace --name "$container_name" -e OPENAI_API_KEY -v "$(pwd):/workdir" "sandbot-devshell:sandbot-devshell" sleep 10000d
+    ${pkgs.podman}/bin/podman start "$container_name"
+    echo "Created container $container_name"
+    echo "You can start it with: sandbot-persistent $container_name"
+  '';
+
+  sandbot-exec = pkgs.writeShellScriptBin "sandbot-exec" ''
+    set -ueo pipefail
+    if [ $# -lt 1 ]; then
+      echo "Usage: $0 <container-name> [command...]"
+      exit 1
+    fi
+    container_name="$1"
+    shift
+    "${pkgs.podman}/bin/podman" exec --tty --interactive "$container_name" "$@"
   '';
 in pkgs.symlinkJoin {
   name = "sandbot";
-  paths = [ sandbot-load sandbot-codex sandbot ];
+  paths = [ sandbot-load sandbot-create sandbot-exec ];
 }
